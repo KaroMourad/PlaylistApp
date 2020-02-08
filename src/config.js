2ea44e02acca1bdd3842538d5b645712948f54d2
Smbat <Smbat.poghosyan@polymorphic.io>
2020-02-08 18:00:08 +0400

    change gitignore

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const branchId = '5e22f30957805011006c728c';
+export const apiUrl = "http://led.idb.idbank.am";
+export const baseUrl = "http://localhost:7000";
+export const HEIGHT = 720;
+export const WIDTH = 1080;
