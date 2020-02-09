2c0d8d40abe1b33821c214a122d1844aaf271b97
Smbat <Smbat.poghosyan@polymorphic.io>
2020-02-09 14:27:21 +0400

    some changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const branchId = '5e22f30957805011006c728c';
-export const apiUrl = "http://led.idb.idbank.am";
+export const branchId = '5e3b0a0e5f7adfda8a72ee3f';
+export const apiUrl = "http://localhost:3000";
 export const baseUrl = "http://localhost:7000";
 export const HEIGHT = 720;
 export const WIDTH = 1080;
